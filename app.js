84b4ecdf157235a9c0c63f1f887ca90a04a64ce9
Robert Glaser <robert.glaser@innoq.com>
2019-02-07 19:04:06 +0100

    must-revalidate

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.get('/shoot', async (req, res) => {
     const screenshot = await takeScreenshot(target, selector, padding)
     if (screenshot) {
       res.type('image/png')
-      res.header('Cache-Control', 's-max-age=600, public')
+      res.header('Cache-Control', 's-max-age=600, public, must-revalidate')
       res.header('ETag', etag(screenshot))
       res.send(screenshot)
     } else {
